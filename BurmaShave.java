007043d272de85a17a47c14b82aff46525bdf2ba
vrushienio <eniovrushi@outlook.com>
2016-09-26 06:07:13 +0200

    Usht.12 fq 49

---
+++ BurmaShave.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+public class BurmaShave
+	{
+		public static void main(String[] args)
+			{
+				JOptionPane.showMessageDialog(null, "Approached a crossing");
+				JOptionPane.showMessageDialog(null, "Without looking");
+				JOptionPane.showMessageDialog(null, "Who will eat");
+				JOptionPane.showMessageDialog(null, "His widow's cooking");
+			}
+}
