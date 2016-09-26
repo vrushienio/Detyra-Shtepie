bae42a962ee8ea61abc957b5a937096fc5d5691e
vrushienio <eniovrushi@outlook.com>
2016-09-26 06:03:32 +0200

    Usht.8 fq 48

---
+++ BigJavaWord.java
@@ -0,0 +1,9 @@
+public class BigJavaWord {
+    public static void main() {
+        System.out.println("          J         A       V      V      A");
+        System.out.println("          J       A   A      V    V      A  A");
+        System.out.println("          J      A     A      V  V     A     A");
+        System.out.println(" J        J     A A A A A      VV     A A A A A");
+        System.out.println("  J JJJJJJ     A         A     V     A         A");
+    }
+}
